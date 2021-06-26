5ae01ddfba7e1ba3964c2cad1edbd43d8f8084ac
Hank Tom <hanktom@gmail.com>
2021-06-27 01:23:48 +0800

    initial commit

---
+++ src/com/tom/Hello.java
@@ -0,0 +1,25 @@
+package com.tom;
+
+import java.util.Random;
+
+public class Hello {
+	
+	public static void main(String[] args) {
+		Random random = new Random();
+		System.out.println("Hello world");
+		System.out.println(220);
+		System.out.println("Please enter your age?");
+		int num = 6;
+		int age = 18;
+		int total = 0;
+		long big = 1123456L;
+		double weight = 65.5;
+		float height = 171.5f;
+		byte b = 12;
+		char c = 'A';
+		char c2 = 123;
+		boolean adult = false;
+		boolean paid = true;
+	}
+	
+}
